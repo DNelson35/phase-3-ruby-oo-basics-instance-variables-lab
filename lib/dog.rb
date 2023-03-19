999202f2ca7c100a3be2356a5fef8f965c600d90
Damien Nelson <dhnisaman@gmail.com>
2023-03-19 19:55:22 -0400

    commit tree 720d6f39808df88c7d32b9aa6fca0e5b69fc96b8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    def name
+        @this_dogs_name
+    end
+
+    def intro 
+       puts "Hi, my name is #{self.name}. Opps I mean Bark Bark!"
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.intro
